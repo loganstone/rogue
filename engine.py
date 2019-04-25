6b41a5c3d7142554e04920e111ed5456258240a6
Hyunsuk Lee <hs.lee.logan@gmail.com>
2019-04-26 00:12:33 +0900

    공격, 데미지, 메시지

--- engine.py
+++ engine.py
@@ -1,12 +1,13 @@
 import tcod as libtcod
-import utils
 
+from components.fighter import Fighter
+from death_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
 from game_states import GameStates
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
-from render_functions import clear_all, render_all
+from render_functions import clear_all, render_all, RenderOrder
 
 
 def main():
@@ -32,7 +33,11 @@ def main():
         'light_ground': libtcod.Color(200, 180, 50),
     }
 
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player',
+                    blocks=True,
+                    render_order=RenderOrder.ACTOR,
+                    fighter=fighter_component)
     entities = [player]
 
     libtcod.console_set_custom_font(
@@ -63,7 +68,7 @@ def main():
             recompute_fov(fov_map, player.x, player.y, fov_radius,
                           fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, game_map, fov_map, fov_recompute,
+        render_all(con, entities, player, game_map, fov_map, fov_recompute,
                    screen_width, screen_height, colors)
 
         libtcod.console_flush()
@@ -76,6 +81,8 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        player_turn_results = []
+
         if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
             destination_x = player.x + dx
@@ -85,10 +92,8 @@ def main():
                 target = get_blocking_entities_at_location(
                     entities, destination_x, destination_y)
                 if target:
-                    print('당신은 ' +
-                          target.name +
-                          utils.pick_nouns(target.name, '을', '를') +
-                          ' 신나게 때려 성가시게 합니다!')
+                    attack_results = player.fighter.attack(target)
+                    player_turn_results.extend(attack_results)
                 else:
                     player.move(dx, dy)
                     fov_recompute = True
@@ -100,13 +105,48 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
+        for player_turn_result in player_turn_results:
+            message = player_turn_result.get('message')
+            dead_entity = player_turn_result.get('dead')
+
+            if message:
+                print(message)
+
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+
+                print(message)
+
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print(entity.name +
-                          utils.pick_nouns(entity.name, '이', '가') +
-                          ' 존재의 의미를 생각해 봅니다.')
-            game_state = GameStates.PLAYERS_TURN
+                if entity.ai:
+                    enemy_turn_results = entity.ai.take_turn(
+                        player, fov_map, game_map, entities)
+
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+
+                        if message:
+                            print(message)
+
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+
+                            print(message)
+
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+                    if game_state == GameStates.PLAYER_DEAD:
+                        break
+            else:
+                game_state = GameStates.PLAYERS_TURN
 
 
 if __name__ == '__main__':
